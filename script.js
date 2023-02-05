31596e6aedca49972bbc5951dde76c247d855d97
wrenbirda <wrenbirda@gmail.com>
2023-02-05 13:27:23 -0800

    Add function for player input 'r'
    It's not working at the moment. I'm pushing this so I can walk away
    and do something else. I don't know why it won't reply, and console
    isn't giving any errors. But like I said, I'll deal with it later.

--- script.js
+++ script.js
@@ -5,20 +5,46 @@ const items = ['Rock', 'Paper', 'Scissors'];
 const getComputerChoice = Math.floor(Math.random() * items.length);
 
 // console.log so I can check it's working.
-console.log(items[getComputerChoice]);
+// console.log(items[getComputerChoice]);
 
 // we need to include a player selection too
 const getPlayerChoice = prompt('Let\'s play rock, paper, scissors! Please type your selection below:');
 
 // console.log so I can check it's working.
-console.log(getPlayerChoice);
+// console.log(getPlayerChoice);
 
 // Now's the scary part. We're going to FIGHT!
+function play() {
+  if (getPlayerChoice == 'r') {
+    if (getComputerChoice == 'Paper') {
+      print= ('You lose! Paper beats rock. Go again.');
+    } else if (getComputerChoice == 'Scissors') {
+      print= ('You win! Rock beats scissors. Go again.');
+    } else if (getComputerChoice == 'Rock') {
+      print= ('You tie! Go again.');
+    }
+  } else {
+    console.log('test');
+  }
+}
 
 
 
+// ignore this shit for now
+// if (getPlayerChoice(rock) && getComputerChoice(paper)) {
+//  console.log('You lose! Paper beats rock.')
+//}
 
 
+
+//function geek() {
+  //var doc = prompt("Please enter some text",
+    //  "GeeksforGeeks");
+//  if (doc != null) {
+  //    document.getElementById("g").innerHTML =
+    //      "Welcome to " + doc;
+ // }
+//}
 
 // Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 // We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
